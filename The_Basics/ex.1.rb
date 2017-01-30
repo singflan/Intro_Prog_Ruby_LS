30d32c057403225eee96f018c27dad6d6ef3ac3e
Dustin Flanary <singflan@gmail.com>
2017-01-30 11:04:45 -0700

    Finished exercise one and started exercise two of The Basics chapter

---
+++ The_Basics/ex.1.rb
@@ -0,0 +1,5 @@
+puts "Enter your first name: "
+first_name = gets.chomp
+puts "Enter your last name: "
+last_name = gets.chomp
+full_name = first_name + " " + last_name
---
+++ The_Basics/ex.2.rb
@@ -0,0 +1,3 @@
+number = 8683
+digit_in_thousands = number / 1000
+puts digit_in_thousands
