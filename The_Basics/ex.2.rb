c6064da1d41d3465caca33f0e19544df6634c4b6
Dustin Flanary <singflan@gmail.com>
2017-01-31 09:58:46 -0700

    Finished exercise 2 from The Basics chapter

--- The_Basics/ex.2.rb
+++ The_Basics/ex.2.rb
@@ -1,3 +1,13 @@
 number = 8683
+# print digit in the thousands place
 digit_in_thousands = number / 1000
 puts digit_in_thousands
+# print digit in the hundreds place
+digit_in_hundreds = number % 1000 / 100
+puts digit_in_hundreds
+# print digit in the tens place
+digit_in_tens = number % 1000 % 100 / 10
+puts digit_in_tens
+# digit in the ones place
+digit_in_ones = number % 1000 % 100 % 10
+puts digit_in_ones
