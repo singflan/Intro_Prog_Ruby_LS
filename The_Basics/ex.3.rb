99639cbb518e0750997d111fe10ca40f7befe64c
Dustin Flanary <singflan@gmail.com>
2017-01-31 11:47:05 -0700

    Added exercises 3 and 4

---
+++ The_Basics/ex.3.rb
@@ -0,0 +1,5 @@
+movies = {"The Lego Movie" => 2014, "Star Wars: A New Hope" => 1977, "Evita" => 1996, "Avatar" => 2009, "Toy Story" => 1995}
+movies.each do |key, value|
+  puts value
+end
+
---
+++ The_Basics/ex4.rb
@@ -0,0 +1,8 @@
+dates = [2014, 1977, 1996, 2009, 1995]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
