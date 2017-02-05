af53e2663f8f7f5fba55e4598620483ba8378219
Dustin Flanary <singflan@gmail.com>
2017-02-05 00:54:49 -0700

    did 1st exercise of Hashes chapter

---
+++ Hashes/ex1_hashes.rb
@@ -0,0 +1,9 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank", "rob", "david"],
+            aunts: ["mary", "sally", "susan"]
+        }
+
+immediate_family = family.select { |k,v| k==:brothers || k==:sisters}
+array = immediate_family.values.flatten
+puts array
