bdb8a86a5decd6145be48a1374dd925ed87740ae
Dustin Flanary <singflan@gmail.com>
2017-02-06 12:28:53 -0700

    Finished exercise 6 on grouping anagrams

--- Hashes/ex6_hashes.rb
+++ Hashes/ex6_hashes.rb
@@ -1,4 +1,17 @@
+result = Hash.new
+
 words = ['demo','none','tied','evil','dome','mode','live','fowl','veil','wolf','diet','vile','edit','tide','flow','neon']
 words.each do |word|
-  
+  key = word.split(//).sort.join
+  if result.has_key?(key)
+    result[key].push(word)
+  else
+    result[key] = [word]
+  end
 end
+
+result.each do |k, v|
+  p v
+end
+
+
